68d555291f6cafac2c1b211db85e97fe4acfa8ac
sunfl025 <dttrang0205@gmail.com>
2025-12-17 17:28:12 +0700

    read

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   root 'application#hello'
+  get "/users", to: "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/users/new", to: "users#new"
+  post "/users", to: "users#create"
 end
 
-Rails.application.routes.draw do 
-  get "/users", to: "users#index"
-end
+
