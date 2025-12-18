96f430a815baa8c016bb2671e02cab65f186109e
sunfl025 <dttrang0205@gmail.com>
2025-12-18 16:58:48 +0700

    crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,49 @@
 class UsersController < ApplicationController
-   def index
-      @users = User.all
-   end
+  before_action :set_user, only: %i[ show edit update destroy ]
 
-   def new
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
     @user = User.new
-   end
+  end
 
-    # def create
-    #   @user = User.new(user_params)
-    #   if @user.save
-    #     redirect_to users_path
-    #   else
-    #     render :new
-    #   end
-    # end
-
-    def create
+  def create
     @user = User.new(user_params)
-        if @user.save
-        redirect_to @user
-        else
-        render :new, status: :unprocessable_entity
-        end
+    if @user.save
+      redirect_to users_path, notice: "User created successfully"
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    private
-    def product_params
-      params.expect(product: [ :name ])
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: "User deleted successfully"
+  end
 
+  private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :sex)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+
   root 'application#hello'
-  get "/users", to: "users#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/users/new", to: "users#new"
-  post "/users", to: "users#create"
+  # get "/users", to: "users#index"
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "/users/new", to: "users#new"
+  # post "/users", to: "users#create"
+  resources :users
+
 end
 
 
